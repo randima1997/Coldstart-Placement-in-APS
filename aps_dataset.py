3c32747be35a47201430dc166c89e7791a478e10
Randima <randimafernando1997@gmail.com>
2026-02-09 05:06:08 +0100

    Tested using L2 norm, did no success

--- aps_dataset.py
+++ aps_dataset.py
@@ -50,9 +50,68 @@ class SiameseNet(nn.Module):
         super().__init__()
         self.emb_net = emb_net
     
-    def forward(self, ft1, ft2, aps1, aps2, sim):
+    def forward(self, ft1, ft2):
 
         z1 = self.emb_net(ft1)
         z2 = self.emb_net(ft2)
 
-        return
+        return z1, z2
+    
+def contrastive_loss(x1, x2, label, margin=2.0):
+    # Calculate L2 distance
+    dist = F.pairwise_distance(x1, x2, p=2)
+    
+    # If label=1 (same), minimize distance. 
+    # If label=0 (different), maximize distance up to margin.
+    loss = (label * torch.pow(dist, 2) + (1 - label) * torch.pow(torch.clamp(margin - dist, min=0.0), 2))
+    
+    return torch.mean(loss)
+
+
+
+def train(train_dataloader, model, device, loss_fn, optim):
+
+    dataset_size = len(train_dataloader.dataset)
+    batch_size = train_dataloader.batch_size
+
+    model.train()
+
+    for batch, (ft1, ft2, aps1, aps2, sim) in enumerate(train_dataloader):
+        ft1, ft2, sim = ft1.to(device), ft2.to(device), sim.to(device)
+
+        z1, z2 = model(ft1, ft2)
+        loss = loss_fn(z1, z2, sim)
+
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+
+        if (batch%10) == 0:
+            train_loss, current = loss.item(), batch * batch_size + len(z1)
+            print(f"loss: {train_loss:>7f}  [{current:>5d}/{dataset_size:>5d}]")
+
+
+def validate(val_dataloader, model, device):
+
+    dataset_size = len(val_dataloader.dataset)
+    results = []
+
+    model.eval()
+
+    with torch.no_grad():
+        for ft1, ft2, aps1, aps2, sim in val_dataloader:
+            
+            
+            ft1, ft2, sim = ft1.to(device), ft2.to(device), sim.to(device)
+
+            z1, z2 = model(ft1, ft2)
+            dist = F.pairwise_distance(z1, z2, p=2)
+
+            for similarity, distance in zip(sim, dist):
+                results.append((round(similarity.item(), 6), round(distance.item(), 6)))
+
+    
+    print("Validation Results : \n")
+
+    for i in results:
+        print(i[0], "\t", i[1])
--- main.py
+++ main.py
@@ -1,5 +1,157 @@
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
+import getpass
+from aps_dataset import APSSiameseDataset, EmbeddingNet, SiameseNet, contrastive_loss, train, validate
+from torch.utils.data import Dataset, DataLoader, random_split
 
 
+# password = getpass.getpass("Enter DB Password: ")
+password = 'SlsFux11'
+engine = create_engine(f"postgresql://randima:{password}@localhost:5432/movielens")
+
+
+# Queries for accessing Database
+
+
+query_complete_d = """
+SELECT
+    performance_results.data_set_name AS dataset,
+    COUNT(performance_results.data_set_name) AS dataset_freq
+FROM
+    performance_results
+    
+GROUP BY
+    performance_results.data_set_name
+HAVING
+    COUNT(performance_results.data_set_name) = 29;
+"""
+query_aps = """
+SELECT
+    performance_results.data_set_name AS dataset,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name = 'BPR') AS BPR,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name  = 'CDAE') AS CDAE,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name  = 'ConvNCF') AS ConvNCF,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name  = 'DGCF') AS DGCF,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name  = 'DiffRec') AS DiffRec,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name  = 'DMF') AS DMF,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name  = 'EASE') AS EASE,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name  = 'ENMF') AS ENMF,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name  = 'FISM') AS FISM,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name  = 'GCMC') AS GCMC,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name  = 'ItemKNN') AS ItemKNN,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name  = 'LDiffRec') AS LDiffRec,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name  = 'LightGCN') AS LightGCN,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name  = 'LINE') AS LINE_alg,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name  = 'MacridVAE') AS MacridVAE,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name  = 'MultiDAE') AS MultiDAE,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name  = 'MultiVAE') AS MultiVAE,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name  = 'NAIS') AS NAIS,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name  = 'NCEPLRec') AS NCEPLRec,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name  = 'NCL') AS NCL,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name  = 'NeuMF') AS NeuMF,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name  = 'NGCF') AS NGCF,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name  = 'NNCF') AS NNCF,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name  = 'Pop') AS Pop,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name  = 'Random') AS Random,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name  = 'RecVAE') AS RecVAE,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name  = 'SGL') AS SGL,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name  = 'SimpleX') AS SimpleX,
+    SUM(performance_results.ndcg5_avg) FILTER (WHERE performance_results.algorithm_name  = 'SpectralCF') AS SpectralCF
+
+FROM
+    performance_results
+GROUP BY
+    performance_results.data_set_name;
+
+"""
+
+query_features = """
+SELECT
+    aps_merged.data_set_name AS dataset,
+    AVG(aps_merged.num_users)::INT AS num_users,
+    AVG(aps_merged.num_items)::INT AS num_items,
+    AVG(aps_merged.num_interactions)::INT AS num_inter,
+    ROUND(AVG(aps_merged.density),6) AS density,
+    ROUND(AVG(aps_merged.user_item_ratio),6) AS u_i_ratio,
+    ROUND(AVG(aps_merged.item_user_ratio),6) AS i_u_ratio,
+    AVG(aps_merged.highest_num_rating_by_single_user)::INT AS highest_num_rating_u,
+    AVG(aps_merged.lowest_num_rating_by_single_user)::INT AS lowest_num_rating_u,
+    AVG(aps_merged.highest_num_rating_on_single_item)::INT AS highest_num_rating_i,
+    AVG(aps_merged.lowest_num_rating_on_single_item)::INT AS lowest_num_rating_i,
+    ROUND(AVG(aps_merged.mean_num_ratings_by_user),6) AS mean_num_rating_u,
+    ROUND(AVG(aps_merged.mean_num_ratings_on_item),6) AS mean_num_rating_i
+FROM
+    aps_merged
+GROUP BY
+    aps_merged.data_set_name;
+"""
+
+
+complete_datasets_df = pd.read_sql(query_complete_d, engine)
+aps_df = pd.read_sql(query_aps, engine).set_index("dataset", drop= False)
+features_df = pd.read_sql(query_features, engine).set_index("dataset", drop= False)
+
+
+mask = complete_datasets_df['dataset'].to_list()
+
+
+aps_vectors = aps_df.loc[mask].reset_index(drop= True)
+feature_vectors = features_df.loc[mask].reset_index(drop= True)
+
+tempaps_df = aps_vectors.iloc[:, 1:]
+temp_df = feature_vectors.iloc[:, 1:]
+t_aps = torch.tensor(tempaps_df.values, dtype=torch.float32)
+t_f = torch.tensor(temp_df.values, dtype=torch.float32)
+
+dataset = APSSiameseDataset(t_f, t_aps)
+
+train_set_size = int(0.95*len(dataset))
+val_set_size = len(dataset) - train_set_size
+
+train_set, val_set = random_split(dataset, [train_set_size, val_set_size])
+
+train_dataloader = DataLoader(
+    train_set,
+    batch_size= 16,
+    shuffle= True,
+    num_workers= 5,
+    pin_memory= True
+)
+
+val_dataloader = DataLoader(
+        val_set,
+        batch_size= 64,
+        shuffle= False,
+        num_workers= 5,
+        pin_memory= True
+)
+
+
+device = (
+            "cuda"
+            if torch.cuda.is_available()
+            else "mps"
+            if torch.backends.mps.is_available()
+            else "cpu"
+        )
+print(f"Using {device} device")
+
+
+
+epochs = 8
+
+model = SiameseNet(EmbeddingNet()).to(device)
+loss_func = contrastive_loss
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+
+for t in range(epochs):
+    print(f"Running Epoch{t}")
+    train(train_dataloader, model, device, loss_func, optimizer)
+
+validate(val_dataloader, model, device)
+
+torch.save(model.state_dict(), 'weights/APSSiamNet_weights_3.pth')
+print("Model saved!")
